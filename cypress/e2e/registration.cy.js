60702e7e0fcea68eaa041a18410fd3a5b58b729c
Piotr Kruszelnicki <arbuzzz1337@gmail.com>
2025-07-08 14:04:48 +0200

    Registration submit

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,64 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
-  before(() => {
+  it('should submit student registration form', () => {
+    cy.visit('https://demoqa.com/automation-practice-form');
 
-  });
+    cy.get('#firstName')
+      .type('Vava');
 
-  it('', () => {
+    cy.get('#lastName')
+      .type('Muffin');
 
+    cy.get('#userEmail')
+      .type('vavamuffin1995@muffin.com');
+
+    cy.get('label[for="gender-radio-1"]')
+      .click();
+
+    cy.get('#userNumber')
+      .type('1234567890');
+
+    cy.get('#dateOfBirthInput')
+      .click();
+
+    cy.get('.react-datepicker__year-select')
+      .select('1995');
+
+    cy.get('.react-datepicker__month-select')
+      .select('May');
+
+    cy.get('.react-datepicker__day--015')
+      .not('.react-datepicker__day--outside-month')
+      .click();
+
+    cy.get('#subjectsInput')
+      .type('Math, English, Algebra');
+
+    cy.contains('label', 'Sports')
+      .click();
+
+    cy.contains('label', 'Music')
+      .click();
+
+    cy.get('#currentAddress')
+      .type('Melbourne 1913-454 Avenue');
+
+    cy.get('#state')
+      .click();
+    cy.get('#react-select-3-input')
+      .type('Haryana{enter}');
+
+    cy.get('#city')
+      .click();
+    cy.get('#react-select-4-input')
+      .type('Panipat{enter}');
+
+    cy.get('#submit')
+      .click();
+
+    cy.get('#example-modal-sizes-title-lg')
+      .should('be.visible')
+      .and('contain', 'Thanks for submitting the form');
   });
 });
